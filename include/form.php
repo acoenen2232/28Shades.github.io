ae5bc65eab495c282f58d8fb8ac0ec327effdd71
acoenen2232 <acoenen2232@gmail.com>
2021-06-25 13:43:31 -0500

    added email

--- include/form.php
+++ include/form.php
@@ -27,8 +27,8 @@ require 'phpmailer/src/SMTP.php';
 $toemails = array();
 
 $toemails[] = array(
-				'email' => 'your-email@website.com', // Your Email Address
-				'name' => 'Your Name' // Your Name
+				'email' => 'art.kane.n@gmail.com', // Your Email Address
+				'name' => 'Ashley Kay' // Your Name
 			);
 
 
@@ -37,8 +37,8 @@ $toemails[] = array(
 ---------------------------------------------------*/
 
 $fromemail = array(
-				'email' => 'no-reply@website.com', // Company's Email Address (preferably currently used Domain Name)
-				'name' => 'Company Name' // Company Name
+				'email' => 'acoenen2232@gmail.com', // Company's Email Address (preferably currently used Domain Name)
+				'name' => 'KaneN' // Company Name
 			);
 
 
